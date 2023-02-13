c5d49b1d52d8511a4dbbb27bd7cbc08625f30501
Ramji <120196523+RamjiSrinivasan-SDE@users.noreply.github.com>
2023-02-13 22:36:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
-
+var btn=document.getElementbyId("btn");
+var numPtag=document.getElementById("num");
+function generateRandomnumber(){
+	
+    var no =Math.floor(Math.random()*40)-20;
+    numPtag.textContent=no;
+}
+btn.addEventListener('click',generateRandomnumber);
